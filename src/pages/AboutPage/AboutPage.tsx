979d44ca8c50bde10848f69071d2f287e8e79415
powyshko <povyshko.y@gmail.com>
2025-03-25 13:36:19 +0300

    роутинг

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,10 @@
+
+const AboutPage = () => {
+  return (
+    <div>
+      About PAGE
+    </div>
+  )
+}
+
+export default AboutPage;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filepath);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@pages": path.resolve(__dirname, "./src/pages")
+    }
+  },
+  build: {
+    target: "esnext",
+    modulePreload: false,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        minifyInternalExports: false,
+      },
+    },
+  },
 })
